c8adf74a55e680f48808c722a85d3ccf9f68a2cf
Ahmadkhaan08 <mrahmadismail96@gmail.com>
2026-04-23 11:11:00 +0500

    first commit

---
+++ server/app.ts
@@ -0,0 +1,29 @@
+import express,{Request,Response,NextFunction} from "express"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+import cors from "cors"
+dotenv.config()
+export const app=express()
+
+
+// body parser
+app.use(express.json({limit:"50mb"}))
+// cookie parser
+app.use(cookieParser())
+// cors
+app.use(cors({
+    origin:process.env.ORIGIN
+}))
+// testing api
+app.get("/test",(req:Request,res:Response,next:NextFunction)=>{
+    res.status(200).json({
+        success:true,
+        message:"Api is working"
+    })
+})
+// unknown routes
+app.use((req:Request,res:Response,next:NextFunction)=>{
+    const err=new Error (`Route ${req.originalUrl} not found`) as any
+    err.statusCode=404
+    next(err)
+})
