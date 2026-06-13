155561ca1d46d9e428470ed03fe73f6bdb661722
Ahmadkhaan08 <mrahmadismail96@gmail.com>
2026-06-13 23:46:43 +0500

    feat: add CourseContent component for managing course content sections and links

--- client/app/components/Admin/Course/CreateCourse.tsx
+++ client/app/components/Admin/Course/CreateCourse.tsx
@@ -4,11 +4,12 @@ import React, { useState } from "react";
 import CourseInformation from "./CourseInformation";
 import CourseOptions from "./CourseOptions";
 import CourseData from "./CourseData";
+import CourseContent from "./CourseContent";
 
 type Props = {};
 
 const CreateCourse = (props: Props) => {
-  const [active, setActive] = useState(1);
+  const [active, setActive] = useState(2);
   const [courseInfo, setCourseInfo] = useState({
     name: "",
     description: "",
@@ -26,7 +27,7 @@ const CreateCourse = (props: Props) => {
       videoUrl: "",
       title: "",
       description: "",
-      videoSection: "",
+      videoSection: "Untitled Section",
       links: [
         {
           title: "",
@@ -38,32 +39,41 @@ const CreateCourse = (props: Props) => {
   ]);
   const [courseData, setCourseData] = useState({});
 
+  const handleSubmit = async () => {};
+
   return (
     <div className="w-full flex min-h-screen">
       <div className="w-[80%]">
-        {
-        active === 0 &&(
-             <CourseInformation
-             courseInfo={courseInfo}
-             setCourseInfo={setCourseInfo}
-             active={active}
-             setActive={setActive} />
-             )}
-             {
-        active === 1 &&(
-             <CourseData
-             benefits={benefits}
-             setBenefits={setBenefits}
-             prerequisities={prerequisities}
-             setPrerequisities={setPrerequisities}
-             active={active}
-             setActive={setActive} />
-             )}
-             </div>
+        {active === 0 && (
+          <CourseInformation
+            courseInfo={courseInfo}
+            setCourseInfo={setCourseInfo}
+            active={active}
+            setActive={setActive}
+          />
+        )}
+        {active === 1 && (
+          <CourseData
+            benefits={benefits}
+            setBenefits={setBenefits}
+            prerequisities={prerequisities}
+            setPrerequisities={setPrerequisities}
+            active={active}
+            setActive={setActive}
+          />
+        )}
+        {active === 2 && (
+          <CourseContent
+            active={active}
+            setActive={setActive}
+            courseContentData={courseContentData}
+            setCourseContentData={setCourseContentData}
+            handleSubmit={handleSubmit}
+          />
+        )}
+      </div>
       <div className="w-[20%] mt-[100px] h-screen fixed z-[-1] top-18 right-0">
-        <CourseOptions 
-        active={active}
-         setActive={setActive} />
+        <CourseOptions active={active} setActive={setActive} />
       </div>
     </div>
   );
